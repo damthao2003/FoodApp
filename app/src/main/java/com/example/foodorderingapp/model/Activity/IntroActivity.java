42d810e3e23490e2248737eb54f8dc95f5aed916
lyvvw <huyduong2192003@gmail.com>
2024-04-08 18:10:43 +0700

    update5

--- app/src/main/java/com/example/foodorderingapp/model/Activity/IntroActivity.java
+++ app/src/main/java/com/example/foodorderingapp/model/Activity/IntroActivity.java
@@ -1,33 +1,59 @@
 package com.example.foodorderingapp.model.Activity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foodorderingapp.R;
-import com.example.foodorderingapp.databinding.ActivityIntroBinding;
+import com.example.foodorderingapp.model.Adaptor.CategoryAdaptor;
+import com.example.foodorderingapp.model.Adaptor.FoodAdaptor;
+import com.example.foodorderingapp.model.Domain.CategoryDomain;
+import com.example.foodorderingapp.model.Domain.FoodDomain;
 
+import java.util.ArrayList;
 
-public class IntroActivity extends BaseActivity {
-    ActivityIntroBinding binding;
+public class IntroActivity extends AppCompatActivity {
+
+    TextView tvLogin;
+    TextView tvSignup;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_intro);
-
-        setVariable();
-//        getWindow().setStatusBarColor(Color.parseColor("FFE4B5"));
-
-    }
-    private void setVariable(){
-        binding.lognInBtn.setOnClickListener(v ->{
-            if(mAuth.getCurrentUser() != null){
-                startActivity(new Intent(IntroActivity.this, MainActivity.class));
-            }else{
-                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+        tvLogin= findViewById(R.id.tvlogin);
+        tvSignup=findViewById(R.id.tvsignup);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                //Toast.makeText(IntroActivity.this, "sdsdfsdfs", Toast.LENGTH_SHORT).show();
 
             }
         });
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 
 }
